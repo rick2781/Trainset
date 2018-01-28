39514d5f593458d78f7cf90f2f6988dd1ee0f298
Rick <ricksofteng@gmail.com>
2018-01-28 15:02:49 -0700

    Update

--- app/src/main/java/rick/trainset/Presentation/AuthActivities/SingInActivity/SignInActivity.java
+++ app/src/main/java/rick/trainset/Presentation/AuthActivities/SingInActivity/SignInActivity.java
@@ -1,9 +1,17 @@
 package rick.trainset.Presentation.AuthActivities.SingInActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import rick.trainset.Presentation.AuthActivities.LoginActivity.LoginActivity;
+import rick.trainset.Presentation.AuthActivities.RegisterActivity.RegisterActivity;
 import rick.trainset.R;
 
 /**
@@ -12,10 +20,49 @@ import rick.trainset.R;
 
 public class SignInActivity extends AppCompatActivity implements SignInContract.View {
 
+    Context context = SignInActivity.this;
+
+    @BindView(R.id.login)
+    Button loginButton;
+
+    @BindView(R.id.register)
+    Button registerButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        ButterKnife.bind(this);
+
+        goLogin();
+        goRegister();
+    }
+
+    //Takes to Login Screen
+    private void goLogin() {
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //Takes to Register Screen
+    private void goRegister() {
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent = new Intent(context, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
